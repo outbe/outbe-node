224b0b39ef738cb468c5126cb954e33b20f68907
golnar-mh <crypgirlmh@gmail.com>
2025-06-20 04:12:39 +0300

    feat: add module constants values

---
+++ x/distribution/constants/constant.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+	Apr              = "0.04"
+	BlockPerYear     = "6307200"
+	MaxSelfbondToken = "1000000"
+	Denom            = "outbe"
+)
+
+const FeeCollectorName = "fee_collector"
+const ModuleName = "distribution"
