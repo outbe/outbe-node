3000b1b69cc70765164dcc1c216e8080153d7f7c
golnar-mh <crypgirlmh@gmail.com>
2025-07-07 11:45:16 +0300

    feat(distribution): integrate custom allocation pool logic into built-in module

--- x/distribution/keeper/abci.go
+++ x/distribution/keeper/abci.go
@@ -3,9 +3,11 @@ package keeper
 import (
 	"context"
 
+	sdkerrors "cosmossdk.io/errors"
 	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	errortypes "github.com/outbe/outbe-node/errors"
 )
 
 // BeginBlocker will persist the current header and validator set as a historical entry
@@ -13,12 +15,20 @@ import (
 func (k WrappedBaseKeeper) BeginBlocker(ctx context.Context) error {
 	defer telemetry.ModuleMeasureSince(stakingtypes.ModuleName, telemetry.Now(), telemetry.MetricKeyBeginBlocker)
 
-	var previousTotalPower int64
-
 	wctx := sdk.UnwrapSDKContext(ctx)
 
-	if err := k.AllocateTokens(ctx, previousTotalPower, wctx.VoteInfos()); err != nil {
-		return err
+	// determine the total power signing the block
+	var previousTotalPower int64
+
+	for _, voteInfo := range wctx.VoteInfos() {
+		previousTotalPower += voteInfo.Validator.Power
 	}
-	return nil
+
+	if err := k.AllocateTokens(ctx, previousTotalPower, wctx.VoteInfos()); err != nil {
+		return sdkerrors.Wrap(errortypes.ErrInvalidRequest, "failed to distribute validator rewards")
+	}
+
+	// record the proposer for when we payout on the next block
+	consAddr := sdk.ConsAddress(wctx.BlockHeader().ProposerAddress)
+	return k.SetPreviousProposerConsAddr(ctx, consAddr)
 }
