7a509eb7df8f62270500be7094ce618d4fa8b223
golnar-mh <crypgirlmh@gmail.com>
2025-07-07 11:50:21 +0300

    feat(gemmint): add staking, reward logic  to the gemmint

--- x/gemmint/types/expected_keepers.go
+++ x/gemmint/types/expected_keepers.go
@@ -5,13 +5,17 @@ import (
 
 	"cosmossdk.io/math"
 
+	abci "github.com/cometbft/cometbft/abci/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	rewardtypes "github.com/outbe/outbe-node/x/reward/types"
 )
 
 // StakingKeeper defines the expected staking keeper
 type StakingKeeper interface {
 	StakingTokenSupply(ctx context.Context) (math.Int, error)
 	BondedRatio(ctx context.Context) (math.LegacyDec, error)
+	GetAllValidators(ctx context.Context) ([]stakingtypes.Validator, error)
 }
 
 // AccountKeeper defines the contract required for account APIs.
@@ -29,6 +33,12 @@ type BankKeeper interface {
 	SendCoinsFromModuleToAccount(ctx context.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromModuleToModule(ctx context.Context, senderModule, recipientModule string, amt sdk.Coins) error
 	MintCoins(ctx context.Context, name string, amt sdk.Coins) error
-
+	GetAllBalances(ctx context.Context, addr sdk.AccAddress) sdk.Coins
 	GetSupply(ctx context.Context, denom string) sdk.Coin
+}
+
+type RewardKeeper interface {
+	GetParams(ctx sdk.Context) (params rewardtypes.Params)
+	GetValidatorSelfBondedTokens(ctx context.Context, val stakingtypes.ValidatorI) (math.LegacyDec, error)
+	CalculateTotalSelfBondedTokens(ctx context.Context, bondedVotes []abci.VoteInfo) (math.Int, error)
 }
