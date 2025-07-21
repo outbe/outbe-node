dd2387994f8d95a38bb3895597c3e3f81a4bf0c2
golnar-mh <crypgirlmh@gmail.com>
2025-07-21 10:36:28 +0300

    utility: add custom params and utility functionalities

---
+++ x/rand/types/expected_keepers.go
@@ -0,0 +1,37 @@
+package types
+
+import (
+	context "context"
+
+	"cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+)
+
+// AccountKeeper defines the expected account keeper used for simulations (noalias)
+type AccountKeeper interface {
+	GetModuleAccount(ctx context.Context, moduleName string) sdk.ModuleAccountI
+	HasAccount(ctx context.Context, addr sdk.AccAddress) bool
+	GetModuleAddress(name string) sdk.AccAddress
+	// Methods imported from account should be defined here
+}
+
+// BankKeeper defines the expected interface needed to retrieve account balances.
+type BankKeeper interface {
+	MintCoins(ctx context.Context, moduleName string, amt sdk.Coins) error
+	BurnCoins(ctx context.Context, moduleName string, amt sdk.Coins) error
+	SendCoinsFromModuleToModule(ctx context.Context, senderModule, recipientModule string, amt sdk.Coins) error
+	SendCoinsFromAccountToModule(ctx context.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	SendCoinsFromModuleToAccount(ctx context.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+	GetAllBalances(ctx context.Context, addr sdk.AccAddress) sdk.Coins
+	GetBalance(ctx context.Context, addr sdk.AccAddress, denom string) sdk.Coin
+	SpendableCoins(ctx context.Context, addr sdk.AccAddress) sdk.Coins
+}
+
+type StakingKeeper interface {
+	StakingTokenSupply(ctx context.Context) (math.Int, error)
+	BondedRatio(ctx context.Context) (math.LegacyDec, error)
+	GetAllValidators(ctx context.Context) ([]stakingtypes.Validator, error)
+	Validator(context.Context, sdk.ValAddress) (stakingtypes.ValidatorI, error)
+	Slash(context.Context, sdk.ConsAddress, int64, int64, math.LegacyDec) (math.Int, error)
+}
